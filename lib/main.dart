fb79e6e45b2e6d7be06d66210de1a0a6aaf58bf2
Lam Nam <nguyenph.nam369@gmail.com>
2023-07-20 10:02:20 +0700

    Quản lý State app Flutter bằng Provider sử dụng MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:hello_world/settings.dart';
+import 'package:hello_world/models/counter.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (_) => MySettings(),
-    child: MaterialApp(
-      home: MyApp(),
-    ),
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => MySettings()),
+      ChangeNotifierProvider(create: (_) => Counter()),
+    ],
+    child: MyApp(),
   ));
-}
-
-class MySettings extends ChangeNotifier {
-  String text = 'Done';
-  Color color = Colors.red;
-
-  void changText() {
-    if (text == 'Hello') {
-      text = 'World';
-    } else {
-      text = 'Hello';
-    }
-    notifyListeners();
-  }
-
-  void changColor() {
-    if (color == Colors.red) {
-      color = Colors.blue;
-    } else {
-      color = Colors.red;
-    }
-    notifyListeners();
-  }
-
-  set newColor(Color newColor) {
-    color = newColor;
-    notifyListeners();
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -43,56 +18,55 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<MySettings>(builder: (context, mySettings, child) {
-      return Scaffold(
+    return MaterialApp(
+      title: 'Flutter Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: context.watch<MySettings>().isDark
+            ? Brightness.dark
+            : Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Provider Demo'),
-          backgroundColor: mySettings.color,
-        ),
-        drawer: Drawer(
-          child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              ElevatedButton(
-                  onPressed: () {
-                    mySettings.changColor();
-                    Navigator.pop(context);
-                  },
-                  child: Text('Chang Color')),
-              ElevatedButton(
-                  onPressed: () {
-                    mySettings.changText();
-                    Navigator.pop(context);
-                  },
-                  child: Text('Chang Text')),
-              ElevatedButton(
-                onPressed: () {
-                  mySettings.newColor = Colors.green;
-                  Navigator.pop(context);
-                },
-                child: Text('Change Color to Green'),
-              ),
-            ]),
-          ),
+          title: Text('Flutter Provider'),
+          actions: [
+            Switch(
+              value: context.watch<MySettings>().isDark,
+              onChanged: (newValue) {
+                //context.read<MySettings>().setBrightness(newValue);
+                Provider.of<MySettings>(context, listen: false)
+                    .setBrightness(newValue);
+              },
+            )
+          ],
         ),
         body: Center(
-            child: Row(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                mySettings.changText();
-                mySettings.changColor();
-              },
-              child: Text('Change Text'),
-              style: ElevatedButton.styleFrom(
-                primary: mySettings.color,
-              ),
-            ),
-            Text('${mySettings.text}')
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text(
+              context.watch<Counter>().myValue.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            )
           ],
         )),
-      );
-    });
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<Counter>().add();
+          },
+          child: Icon(Icons.add),
+        ));
   }
 }
