7ff90fec4609cc203ad6c9ad559ac9762f8ab799
Lam Nam <nguyenph.nam369@gmail.com>
2023-07-21 09:20:05 +0700

    Hướng dẫn sử dụng GetX để quản lý trạng thái cho Flutter (GetX State Management Flutter)

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => GioiTinh()),
-      ChangeNotifierProvider(create: (_) => BangCap()),
-    ],
-    child: MaterialApp(home: MyApp()),
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
-enum gioi_tinh { nam, nu }
-
-enum bang_cap { CaoDang, DaiHoc, ThacSi, TienSi }
-
-class GioiTinh extends ChangeNotifier {
-  gioi_tinh? _gioiTinh = gioi_tinh.nam;
-
-  gioi_tinh? get gioiTinh => _gioiTinh;
-
-  set gioiTinh(gioi_tinh? value) {
-    _gioiTinh = value;
-    notifyListeners();
-  }
-}
-
-class BangCap extends ChangeNotifier {
-  bang_cap? _bangCap = bang_cap.DaiHoc;
-
-  bang_cap? get bangCap => _bangCap;
-
-  set bangCap(value) {
-    _bangCap = value;
-    notifyListeners();
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class Home extends StatelessWidget {
+  var count = 0.obs;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Radio Demo'),
+        title: Text('Counter App'),
       ),
-      body: Consumer2<GioiTinh, BangCap>(
-          builder: (context, infoGioiTinh, infoBangCap, child) {
-        return Column(
-          children: [
-            Text('Gioi Tinh'),
-            RadioListTile<gioi_tinh?>(
-              value: gioi_tinh.nam,
-              title: Text('Nam'),
-              secondary: Icon(Icons.male),
-              groupValue: infoGioiTinh.gioiTinh,
-              onChanged: (value) {
-                infoGioiTinh.gioiTinh = value;
-              },
-            ),
-            RadioListTile<gioi_tinh?>(
-              value: gioi_tinh.nu,
-              title: Text('Nu'),
-              secondary: Icon(Icons.female),
-              groupValue: infoGioiTinh.gioiTinh,
-              onChanged: (value) {
-                infoGioiTinh.gioiTinh = value;
-              },
-            ),
-            Text('Bang Cap'),
-            RadioListTile<bang_cap?>(
-              value: bang_cap.CaoDang,
-              title: Text('Cao Dang'),
-              groupValue: infoBangCap.bangCap,
-              onChanged: (value) {
-                infoBangCap.bangCap = value;
-              },
-            ),
-            RadioListTile<bang_cap?>(
-              value: bang_cap.DaiHoc,
-              title: Text('Dai Hoc'),
-              groupValue: infoBangCap.bangCap,
-              onChanged: (value) {
-                infoBangCap.bangCap = value;
-              },
-            ),
-            RadioListTile<bang_cap?>(
-              value: bang_cap.TienSi,
-              title: Text('Tien Si'),
-              groupValue: infoBangCap.bangCap,
-              onChanged: (value) {
-                infoBangCap.bangCap = value;
-              },
-            ),
-            RadioListTile<bang_cap?>(
-              value: bang_cap.ThacSi,
-              title: Text('Thac Si'),
-              groupValue: infoBangCap.bangCap,
-              onChanged: (value) {
-                infoBangCap.bangCap = value;
-              },
-            ),
-            Divider(
-              height: 100,
-            ),
-            Center(
-              child: Text(
-                'Thong tin ca nhan: ${infoGioiTinh.gioiTinh}, ${infoBangCap.bangCap}',
-                style: TextStyle(fontSize: 20),
-              ),
-            )
-          ],
-        );
-      }),
+      body: Center(
+        child: Obx(
+          () => Text(
+            '$count',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => count++,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
